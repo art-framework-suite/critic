88f9f3634121d3b2beec0489f704882b2df32b6b
Marc Paterno <paterno@fnal.gov>
2017-04-10 16:11:31 -0500

    Add lots of const qualifiers

--- critic/test/gallery/gallery_t.cc
+++ critic/test/gallery/gallery_t.cc
@@ -35,90 +35,92 @@
 
 int main() {
 
-  art::InputTag inputTagTriggerResults("TriggerResults", "", "PROD1");
-  art::InputTag inputTagIncorrect("IncorrectLabel", "IncorrectInstance", "PROD1");
-  art::InputTag inputTagEventID("m1", "eventID", "PROD1");
+  art::InputTag const  inputTagTriggerResults("TriggerResults", "", "PROD1");
+  art::InputTag const  inputTagIncorrect("IncorrectLabel", "IncorrectInstance", "PROD1");
+  art::InputTag const  inputTagEventID("m1", "eventID", "PROD1");
 
-  art::InputTag inputTag111(std::string("m1::PROD1"));
-  art::InputTag inputTag121("m1:i2:PROD1");
-  art::InputTag inputTag131(std::string("m1"), std::string("i3"), std::string("PROD1"));
+  art::InputTag const  inputTag111(std::string("m1::PROD1"));
+  art::InputTag const  inputTag121("m1:i2:PROD1");
+  art::InputTag const  inputTag131(std::string("m1"), std::string("i3"), std::string("PROD1"));
 
-  art::InputTag inputTags111("m1", "", "PROD1");
-  art::InputTag inputTags121("m1", "i2", "PROD1");
-  art::InputTag inputTags131("m1", "i3", "PROD1");
+  art::InputTag const  inputTags111("m1", "", "PROD1");
+  art::InputTag const  inputTags121("m1", "i2", "PROD1");
+  art::InputTag const  inputTags131("m1", "i3", "PROD1");
 
-  art::InputTag inputTag211("m2", "", "PROD1");
-  art::InputTag inputTag221("m2", "i2", "PROD1");
+  art::InputTag const  inputTag211("m2", "", "PROD1");
+  art::InputTag const  inputTag221("m2", "i2", "PROD1");
 
-  art::InputTag inputTag112("m1", "", "PROD2");
-  art::InputTag inputTag122("m1", "i2", "PROD2");
+  art::InputTag const  inputTag112("m1", "", "PROD2");
+  art::InputTag const  inputTag122("m1", "i2", "PROD2");
 
-  art::InputTag inputTag113("m1", "", "PROD3");
-  art::InputTag inputTag123("m1", "i2", "PROD3");
+  art::InputTag const  inputTag113("m1", "", "PROD3");
+  art::InputTag const  inputTag123("m1", "i2", "PROD3");
 
-  art::InputTag inputTag11("m1");
-  art::InputTag inputTag12("m1:i2");
+  art::InputTag const  inputTag11("m1");
+  art::InputTag const  inputTag12("m1:i2");
 
-  art::InputTag inputTags11("m1", "");
-  art::InputTag inputTags12("m1", "i2");
+  art::InputTag const  inputTags11("m1", "");
+  art::InputTag const  inputTags12("m1", "i2");
 
-  art::InputTag inputTag21("m2", "");
-  art::InputTag inputTag22("m2", "i2");
+  art::InputTag const  inputTag21("m2", "");
+  art::InputTag const  inputTag22("m2", "i2");
 
-  art::InputTag inputTag31("m3", "");
-  art::InputTag inputTag32("m3", "i2");
+  art::InputTag const  inputTag31("m3", "");
+  art::InputTag const  inputTag32("m3", "i2");
 
-  art::InputTag inputTag312("m3", "", "PROD2");
-  art::InputTag inputTag322("m3", "i2", "PROD2");
+  art::InputTag const  inputTag312("m3", "", "PROD2");
+  art::InputTag const  inputTag322("m3", "i2", "PROD2");
 
-  art::InputTag inputTag41("m4", "");
-  art::InputTag inputTag42("m4", "i2");
+  art::InputTag const  inputTag41("m4", "");
+  art::InputTag const  inputTag42("m4", "i2");
 
-  art::InputTag inputTag411("m4", "", "PROD1");
-  art::InputTag inputTag421("m4", "i2", "PROD1");
+  art::InputTag const  inputTag411("m4", "", "PROD1");
+  art::InputTag const  inputTag421("m4", "i2", "PROD1");
 
-  art::InputTag inputTag51("m5", "");
-  art::InputTag inputTag52("m5", "i2");
+  art::InputTag const  inputTag51("m5", "");
+  art::InputTag const  inputTag52("m5", "i2");
 
-  art::InputTag inputTags61("m6", "");
-  art::InputTag inputTags62("m6", "i2");
+  art::InputTag const  inputTags61("m6", "");
+  art::InputTag const  inputTags62("m6", "i2");
 
-  art::InputTag inputTagFile2(std::string("m0::PROD3"));
+  art::InputTag const  inputTagFile2(std::string("m0::PROD3"));
 
-  art::InputTag inputTagPtrTest("ptr1");
+  art::InputTag const  inputTagPtrTest("ptr1");
 
-  art::InputTag inputTagAssnTest1("ptr1");
-  art::InputTag inputTagAssnTest2("ptr1::PROD3");
+  art::InputTag const  inputTagAssnTest1("ptr1");
+  art::InputTag const  inputTagAssnTest2("ptr1::PROD3");
 
   // Test InputTag
   assert(inputTag121 != inputTags131);
   assert(inputTag121 != inputTag122);
   assert(inputTag122 != inputTag322);
-  art::InputTag inputTagTest("m3", "i2", "PROD2");
+  art::InputTag const  inputTagTest("m3", "i2", "PROD2");
   assert(!(inputTag322 != inputTagTest));
   std::cout << inputTagTest << std::endl;
 
   // Test TypeLabelInstance class
-  art::TypeID typeIntProduct(typeid(arttest::IntProduct));
-  art::TypeID typeStringProduct(typeid(arttest::StringProduct));
-  gallery::TypeLabelInstanceKey key1(typeIntProduct, "a", "b");
+  art::TypeID const typeIntProduct(typeid(arttest::IntProduct));
+  art::TypeID const typeStringProduct(typeid(arttest::StringProduct));
+  gallery::TypeLabelInstanceKey const key1(typeIntProduct, "a", "b");
   assert(key1.typeID() == typeIntProduct &&
          std::string(key1.label()) == "a" &&
          std::string(key1.instance()) == "b");
-  gallery::TypeLabelInstanceKey key2(typeStringProduct, "a", "b");
-  gallery::TypeLabelInstanceKey key3(typeIntProduct, "c", "b");
-  gallery::TypeLabelInstanceKey key4(typeIntProduct, "a", "c");
+  gallery::TypeLabelInstanceKey const key2(typeStringProduct, "a", "b");
+  gallery::TypeLabelInstanceKey const key3(typeIntProduct, "c", "b");
+  gallery::TypeLabelInstanceKey const key4(typeIntProduct, "a", "c");
   assert(key1 < key2 || key2 < key1);
   assert(key1 < key3 && key1 < key4);
 
   // Read values from a ROOT file and test that we get the correct
   // products with the arbitrary values we know were put into them.
-  std::vector<std::string> filenames {"../gallery_makeInput8.d/test_gallery8.root",
-                                      "../gallery_makeInput5.d/test_gallery5.root",
-                                      "../gallery_makeInput8.d/test_gallery8.root",
-                                      "../gallery_makeInput7.d/test_gallery7.root",
-                                      "../gallery_makeInput7.d/test_gallery7.root",
-                                      "../gallery_makeInput8.d/test_gallery8.root"};
+  std::vector<std::string> const filenames {
+      "../gallery_makeInput8.d/test_gallery8.root",
+      "../gallery_makeInput5.d/test_gallery5.root",
+      "../gallery_makeInput8.d/test_gallery8.root",
+      "../gallery_makeInput7.d/test_gallery7.root",
+      "../gallery_makeInput7.d/test_gallery7.root",
+      "../gallery_makeInput8.d/test_gallery8.root"
+  };
 
   gallery::Event ev(filenames, true, 1);
   assert(ev.numberOfEventsInFile() == 10);
@@ -127,10 +129,14 @@ int main() {
   unsigned int counter = 0;
   for( ; ! ev.atEnd(); ev.next(), ++iEvent, ++counter) {
     assert(ev.isValid());
-    assert(ev.eventEntry() == (counter < 15 ? counter % 10 : counter % 5));
+    // First non-empty file has 10 events, other non-empty file has 5 events.
+    assert(ev.eventEntry() == (counter < 10 ? counter % 10 : counter % 5));
 
-    if (counter / 10 == 0) assert(ev.fileEntry() == 1);
-    else assert(ev.fileEntry() == (counter < 15 ? 3 : 4));
+    if (counter / 10 == 0) {
+      assert(ev.fileEntry() == 1);
+    } else {
+      assert(ev.fileEntry() == (counter < 15 ? 3 : 4));
+    }
 
     art::EventAuxiliary const& aux = ev.eventAuxiliary();
     std::cout << aux.id() << "\n";
@@ -149,8 +155,8 @@ int main() {
     assert(ev.getByLabel(inputTagTriggerResults, triggerResultsHandle));
     assert(triggerResultsHandle.isValid());
     assert(!triggerResultsHandle.whyFailed());
-    std::string test1 = triggerResultsHandle->parameterSetID().to_string();
-    std::string test2 = (*triggerResultsHandle).parameterSetID().to_string();
+    std::string const test1 = triggerResultsHandle->parameterSetID().to_string();
+    std::string const test2 = (*triggerResultsHandle).parameterSetID().to_string();
     assert(!ev.getByLabel(inputTagIncorrect, triggerResultsHandle));
     assert(!triggerResultsHandle.isValid());
     assert(triggerResultsHandle.whyFailed());
@@ -162,114 +168,114 @@ int main() {
     }
     assert(exceptionWasThrown);
 
-    auto triggerResults = ev.getValidHandle<art::TriggerResults>(inputTagTriggerResults);
+    auto const triggerResults = ev.getValidHandle<art::TriggerResults>(inputTagTriggerResults);
     std::cout << "psetID = " <<  triggerResults->parameterSetID().to_string() << "\n";
-    std::string test3 = triggerResults->parameterSetID().to_string();
+    std::string const test3 = triggerResults->parameterSetID().to_string();
     assert(test1 == test3);
     assert(test2 == test3);
 
-    auto eventIDInt = ev.getValidHandle<arttest::IntProduct>(inputTagEventID);
+    auto const eventIDInt = ev.getValidHandle<arttest::IntProduct>(inputTagEventID);
     assert(static_cast<unsigned int>(eventIDInt->value) == aux.id().event());
 
-    auto intProduct111 = ev.getValidHandle<arttest::IntProduct>(inputTag111);
+    auto const intProduct111 = ev.getValidHandle<arttest::IntProduct>(inputTag111);
     assert(intProduct111->value == 111);
 
-    auto intProduct121 = ev.getValidHandle<arttest::IntProduct>(inputTag121);
+    auto const intProduct121 = ev.getValidHandle<arttest::IntProduct>(inputTag121);
     assert((*intProduct121).value == 121);
 
-    auto intProduct131 = ev.getValidHandle<arttest::IntProduct>(inputTag131);
+    auto const intProduct131 = ev.getValidHandle<arttest::IntProduct>(inputTag131);
     assert(intProduct131.product()->value == 131);
 
-    auto stringProduct111 = ev.getValidHandle<arttest::StringProduct>(inputTags111);
+    auto const stringProduct111 = ev.getValidHandle<arttest::StringProduct>(inputTags111);
     assert(stringProduct111->name_ == "s111");
 
-    auto stringProduct121 = ev.getValidHandle<arttest::StringProduct>(inputTags121);
+    auto const stringProduct121 = ev.getValidHandle<arttest::StringProduct>(inputTags121);
     assert(stringProduct121->name_ == "s121");
 
-    auto stringProduct131 = ev.getValidHandle<arttest::StringProduct>(inputTags131);
+    auto const stringProduct131 = ev.getValidHandle<arttest::StringProduct>(inputTags131);
     assert(stringProduct131->name_ == "s131");
 
-    auto intProduct211 = ev.getValidHandle<arttest::IntProduct>(inputTag211);
+    auto const intProduct211 = ev.getValidHandle<arttest::IntProduct>(inputTag211);
     assert(intProduct211->value == 211);
 
-    auto intProduct221 = ev.getValidHandle<arttest::IntProduct>(inputTag221);
+    auto const intProduct221 = ev.getValidHandle<arttest::IntProduct>(inputTag221);
     assert(intProduct221->value == 221);
 
-    auto intProduct112 = ev.getValidHandle<arttest::IntProduct>(inputTag112);
+    auto const intProduct112 = ev.getValidHandle<arttest::IntProduct>(inputTag112);
     assert(intProduct112->value == 112);
 
-    auto intProduct122 = ev.getValidHandle<arttest::IntProduct>(inputTag122);
+    auto const intProduct122 = ev.getValidHandle<arttest::IntProduct>(inputTag122);
     assert(intProduct122->value == 122);
 
-    auto intProduct113 = ev.getValidHandle<arttest::IntProduct>(inputTag113);
+    auto const intProduct113 = ev.getValidHandle<arttest::IntProduct>(inputTag113);
     assert(intProduct113->value == 113);
 
-    auto intProduct123 = ev.getValidHandle<arttest::IntProduct>(inputTag123);
+    auto const intProduct123 = ev.getValidHandle<arttest::IntProduct>(inputTag123);
     assert(intProduct123->value == 123);
 
-    auto intProduct11 = ev.getValidHandle<arttest::IntProduct>(inputTag11);
+    auto const intProduct11 = ev.getValidHandle<arttest::IntProduct>(inputTag11);
     assert(intProduct11->value == 113);
 
-    auto intProduct12 = ev.getValidHandle<arttest::IntProduct>(inputTag12);
+    auto const intProduct12 = ev.getValidHandle<arttest::IntProduct>(inputTag12);
     assert(intProduct12->value == 123);
 
-    auto stringProduct11 = ev.getValidHandle<arttest::StringProduct>(inputTags11);
+    auto const stringProduct11 = ev.getValidHandle<arttest::StringProduct>(inputTags11);
     assert(stringProduct11->name_ == "s113");
 
-    auto stringProduct12 = ev.getValidHandle<arttest::StringProduct>(inputTags12);
+    auto const stringProduct12 = ev.getValidHandle<arttest::StringProduct>(inputTags12);
     assert(stringProduct12->name_ == "s123");
 
-    auto intProduct21 = ev.getValidHandle<arttest::IntProduct>(inputTag21);
+    auto const intProduct21 = ev.getValidHandle<arttest::IntProduct>(inputTag21);
     assert(intProduct21->value == 213);
 
-    auto intProduct22 = ev.getValidHandle<arttest::IntProduct>(inputTag22);
+    auto const intProduct22 = ev.getValidHandle<arttest::IntProduct>(inputTag22);
     assert(intProduct22->value == 223);
 
-    auto intProduct31 = ev.getValidHandle<arttest::IntProduct>(inputTag31);
+    auto const intProduct31 = ev.getValidHandle<arttest::IntProduct>(inputTag31);
     assert(intProduct31->value == 312);
 
-    auto intProduct32 = ev.getValidHandle<arttest::IntProduct>(inputTag32);
+    auto const intProduct32 = ev.getValidHandle<arttest::IntProduct>(inputTag32);
     assert(intProduct32->value == 322);
 
-    auto intProduct312 = ev.getValidHandle<arttest::IntProduct>(inputTag312);
+    auto const intProduct312 = ev.getValidHandle<arttest::IntProduct>(inputTag312);
     assert(intProduct312->value == 312);
 
-    auto intProduct322 = ev.getValidHandle<arttest::IntProduct>(inputTag322);
+    auto const intProduct322 = ev.getValidHandle<arttest::IntProduct>(inputTag322);
     assert(intProduct322->value == 322);
 
-    auto intProduct41 = ev.getValidHandle<arttest::IntProduct>(inputTag41);
+    auto const intProduct41 = ev.getValidHandle<arttest::IntProduct>(inputTag41);
     assert(intProduct41->value == 411);
 
-    auto intProduct42 = ev.getValidHandle<arttest::IntProduct>(inputTag42);
+    auto const intProduct42 = ev.getValidHandle<arttest::IntProduct>(inputTag42);
     assert(intProduct42->value == 421);
 
-    auto intProduct411 = ev.getValidHandle<arttest::IntProduct>(inputTag411);
+    auto const intProduct411 = ev.getValidHandle<arttest::IntProduct>(inputTag411);
     assert(intProduct411->value == 411);
 
-    auto intProduct421 = ev.getValidHandle<arttest::IntProduct>(inputTag421);
+    auto const intProduct421 = ev.getValidHandle<arttest::IntProduct>(inputTag421);
     assert(intProduct421->value == 421);
 
 
-    auto intProduct51 = ev.getValidHandle<arttest::IntProduct>(inputTag51);
+    auto const intProduct51 = ev.getValidHandle<arttest::IntProduct>(inputTag51);
     assert(intProduct51->value == 512);
 
-    auto intProduct52 = ev.getValidHandle<arttest::IntProduct>(inputTag52);
+    auto const intProduct52 = ev.getValidHandle<arttest::IntProduct>(inputTag52);
     assert(intProduct52->value == 522);
 
-    auto stringProduct61 = ev.getValidHandle<arttest::StringProduct>(inputTags61);
+    auto const stringProduct61 = ev.getValidHandle<arttest::StringProduct>(inputTags61);
     assert(stringProduct61->name_ == "s611");
 
-    auto stringProduct62 = ev.getValidHandle<arttest::StringProduct>(inputTags62);
+    auto const stringProduct62 = ev.getValidHandle<arttest::StringProduct>(inputTags62);
     assert(stringProduct62->name_ == "s621");
 
     if (counter > 9) {
-      auto intProductFile2 = ev.getValidHandle<arttest::IntProduct>(inputTagFile2);
+      auto const intProductFile2 = ev.getValidHandle<arttest::IntProduct>(inputTagFile2);
       assert(intProductFile2->value == 2000);
     }
 
-    auto ptrTestProduct = ev.getValidHandle<critictest::LitePtrTestProduct>(inputTagPtrTest);
+    auto const ptrTestProduct = ev.getValidHandle<critictest::LitePtrTestProduct>(inputTagPtrTest);
 
-    int eventInt = static_cast<int>(aux.id().event());
+    int const eventInt = static_cast<int>(aux.id().event());
     assert(*ptrTestProduct->ptrInt1 == 111 + eventInt);
     assert(ptrTestProduct->ptrInt1.isAvailable());
     assert(*ptrTestProduct->ptrInt2 == 122);
@@ -307,7 +313,7 @@ int main() {
 
     bool exceptionThrown = true;
     try {
-      auto x = *ptrTestProduct->ptrIntoContainerToBeDropped;
+      auto const x = *ptrTestProduct->ptrIntoContainerToBeDropped;
       std::cout << x << "\n";
       exceptionThrown = false;
     } catch (art::Exception const&) {
@@ -401,7 +407,7 @@ int main() {
     ev.getByLabel(inputTag111, hVStringProduct);
     assert(hVStringProduct.isValid());
 
-    art::FindOne<int, critictest::LiteAssnTestData> findOne(hVStringProduct, ev, inputTagAssnTest1);
+    art::FindOne<int, critictest::LiteAssnTestData> const findOne(hVStringProduct, ev, inputTagAssnTest1);
     assert(findOne.at(0).isValid());
     if (ev.fileEntry() == 1) {
       assert(findOne.at(0).ref() == 121);
@@ -419,7 +425,7 @@ int main() {
     ev.getByLabel(inputTag111, hB);
     assert(hB.isValid());
 
-    art::FindOne<arttest::StringProduct, critictest::LiteAssnTestData> findOneBA(hB, ev, inputTagAssnTest1);
+    art::FindOne<arttest::StringProduct, critictest::LiteAssnTestData> const findOneBA(hB, ev, inputTagAssnTest1);
     assert(findOneBA.at(1).isValid());
     if (ev.fileEntry() == 1) {
       assert(findOneBA.at(1).ref() == arttest::StringProduct(std::string("s111")));
