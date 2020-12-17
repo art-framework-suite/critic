7576d16791a68a93f85f8d3b678e2e62cc049e6a
Kyle Knoepfel <knoepfel@fnal.gov>
2020-12-17 13:43:38 -0600

    Add missing header dependencies.

--- critic/test/art/TestBitsOutput_module.cc
+++ critic/test/art/TestBitsOutput_module.cc
@@ -1,5 +1,6 @@
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Core/OutputModule.h"
+#include "art/Framework/Principal/Event.h"
 #include "art/Framework/Principal/Handle.h"
 #include "art/Framework/Services/Registry/ServiceHandle.h"
 #include "art/Framework/Services/System/TriggerNamesService.h"
