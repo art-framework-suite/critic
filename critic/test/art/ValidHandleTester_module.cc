95521f8b53344382585fc87b2fc814fbb5397301
Kyle Knoepfel <knoepfel@fnal.gov>
2021-02-12 11:28:56 -0600

    Small adjustments to ValidHandle test.

--- critic/test/art/ValidHandleTester_module.cc
+++ critic/test/art/ValidHandleTester_module.cc
@@ -18,7 +18,6 @@
 #include "fhiclcpp/ParameterSet.h"
 
 class ValidHandleTester : public art::EDAnalyzer {
-
   struct Config {
     fhicl::Atom<std::string> input_label{fhicl::Name{"input_label"}};
     fhicl::Atom<std::string> expected_value{fhicl::Name{"expected_value"}};
@@ -27,32 +26,31 @@ class ValidHandleTester : public art::EDAnalyzer {
 public:
   using Parameters = art::EDAnalyzer::Table<Config>;
   explicit ValidHandleTester(Parameters const& p);
-  void analyze(art::Event const& e) override;
 
 private:
-  art::InputTag input_tag_;
+  void analyze(art::Event const& e) override;
+
+  art::ProductToken<std::string> token_;
   std::string expected_value_;
 };
 
 ValidHandleTester::ValidHandleTester(Parameters const& ps)
   : art::EDAnalyzer{ps}
-  , input_tag_{ps().input_label()}
+  , token_{consumes<std::string>(ps().input_label())}
   , expected_value_{ps().expected_value()}
-{
-  // consumes<std::string>(input_tag_);
-}
+{}
 
 void
 ValidHandleTester::analyze(art::Event const& e)
 {
   // Make sure old-form access works.
   art::Handle<std::string> h;
-  e.getByLabel(input_tag_, h);
+  e.getByToken(token_, h);
   assert(h.isValid());
   assert(*h == expected_value_);
 
   // Make sure new-form access works.
-  auto p = e.getValidHandle<std::string>(input_tag_);
+  auto p = e.getValidHandle(token_);
   assert(*p == expected_value_);
   auto const& prov = *p.provenance();
   assert(prov == *h.provenance());
@@ -64,7 +62,9 @@ ValidHandleTester::analyze(art::Event const& e)
   assert(tag.process() == prov.processName());
 
   // Make sure conversion-to-pointer works.
-  [&](std::string const* x) { assert(*x == expected_value_); }(p);
+  assert(p != nullptr);
+  std::string const* x{p};
+  assert(*x == expected_value_);
 }
 
 DEFINE_ART_MODULE(ValidHandleTester)
