8f62e731dff01652044c81c94747f5bed660a70b
Kyle Knoepfel <knoepfel@fnal.gov>
2021-04-28 16:45:10 -0500

    Avoid using Boost unit_test for MT jobs.

--- critic/test/art/CheckPathResults_module.cc
+++ critic/test/art/CheckPathResults_module.cc
@@ -1,11 +1,9 @@
-#include "boost/test/unit_test.hpp"
-
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Core/SharedAnalyzer.h"
 #include "art/Framework/Principal/Event.h"
 #include "art/Framework/Services/System/TriggerNamesService.h"
 
-#include <iostream>
+#include <cassert>
 
 namespace {
   class CheckPathResults : public art::SharedAnalyzer {
@@ -43,16 +41,16 @@ namespace {
   CheckPathResults::analyze(art::Event const& e, art::ProcessingFrame const&)
   {
     auto const& tr = triggerNames_->triggerResults(e, processName_);
-    BOOST_TEST(tr.size() == 3ull);
-    BOOST_TEST(tr.accept());
+    assert(tr.size() == 3ull);
+    assert(tr.accept());
 
     auto const results = triggerNames_->pathResults(e, processName_);
     bool const should_pass_event = std::binary_search(
       begin(expectedEvents_), end(expectedEvents_), e.event());
-    BOOST_TEST(size(results) == 3ull);
-    BOOST_TEST(results.at("p1").accept() == should_pass_event);
-    BOOST_TEST(results.at("p2").accept());
-    BOOST_TEST(results.at("p3").accept() == not should_pass_event);
+    assert(size(results) == 3ull);
+    assert(results.at("p1").accept() == should_pass_event);
+    assert(results.at("p2").accept());
+    assert(results.at("p3").accept() == not should_pass_event);
   }
 }
 
