b0bf39ab83d7f2c08197304d90ce8f95e1e47641
Kyle Knoepfel <knoepfel@fnal.gov>
2021-06-11 09:56:30 -0500

    Test module destructors are run at end of job.

---
+++ critic/test/art/EnsureNoModulesAtEndOfJob_service.cc
@@ -0,0 +1,4 @@
+#include "art/Framework/Services/Registry/ServiceDefinitionMacros.h"
+#include "critic/test/art/EnsureNoModulesAtEndOfJob.h"
+
+DEFINE_ART_SERVICE(art::test::EnsureNoModulesAtEndOfJob)
